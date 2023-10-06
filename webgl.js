9deb1ee78aee429a95f58a88338feb9f2e9796fe
caleb-school-project <147189781+caleb-school-project@users.noreply.github.com>
2023-10-06 11:44:44 -0600

    Create webgl.js

---
+++ webgl.js
@@ -0,0 +1,25 @@
+// copied from https://developer.mozilla.org/en-US/docs/Web/API/WebGL_API/Tutorial/Getting_started_with_WebGL
+
+main();
+
+//
+// start here
+//
+function main() {
+  const canvas = document.querySelector("#glcanvas");
+  // Initialize the GL context
+  const gl = canvas.getContext("webgl");
+
+  // Only continue if WebGL is available and working
+  if (gl === null) {
+    alert(
+      "Unable to initialize WebGL. Your browser or machine may not support it.",
+    );
+    return;
+  }
+
+  // Set clear color to black, fully opaque
+  gl.clearColor(0.0, 0.0, 0.0, 1.0);
+  // Clear the color buffer with specified clear color
+  gl.clear(gl.COLOR_BUFFER_BIT);
+}
