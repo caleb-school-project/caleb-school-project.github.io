da992c004b943bbec89f04b2c1c84fb3a0b96642
Caleb Jeffries <caleb.jeffries.2010@gmail.com>
2024-01-11 22:55:23 -0400

    Completely reorganize code to be more asyncronous

--- webgl.js
+++ webgl.js
@@ -1,78 +1,14 @@
 // Main webGL function
 
-function getFile(file) {
-  xhr = new XMLHttpRequest();
-  xhr.open("GET", file, false);
-  xhr.onLoad = function() {
-    return xhr.responseText;
-  }
-  xhr.send();
-}
+// Start when the page has loaded
+window.onload = main;
 
-function createShader(webgl, source, type) {
-  const shader = webgl.createShader(type);
-  webgl.shaderSource(shader, source);
-  webgl.compileShader(shader);
-  return shader;
-}
-
-function drawSquare() {
-  // Get the shaders
-  xhr = new XMLHttpRequest();
-  xhr.open("GET", "vertex.glsl", false);
-  xhr.onreadystatechange = function() {
-    if (this.readystate === 4 && this.status === 200) {
-      var vertexShaderSource = xhr.responseText;
-      var vertexShader = createShader(webGL, vertexShaderSource, webGL.VERTEX_SHADER);
-      getFrag();
-    }
-  }
-  xhr.send();
-  function getFrag() {
-    xhr = new XMLHttpRequest();
-    xhr.open("GET", "fragment.glsl", false);
-    xhr.onreadystatechange = function() {
-      if (this.readystate === 4 && this.status === 200) {
-        var fragmentShaderSource = xhr.responseText;
-        var fragmentShader = createShader(webGL, fragmentShaderSource, webGL.FRAGMENT_SHADER);
-        draw();
-      }
-    }
-  }
-  function draw() {
-    program = webGL.createProgram();
-    webGL.attachShader(program, vertexShader);
-    webGL.attachShader(program, fragmentShader);
-    webGL.linkProgram(program);
-  
-      // Coordonates for the square
-    squareVertices = new Float32Array([
-      -0.5, 0.5, -0.5, -0.5, 0.5, -0.5
-      -0.5, 0.5, 0.5, 0.5, 0.5, -0.5
-    ]);
-    var dimensions = 2;
-    var itemNum = squareVertices.length / dimensions;
-    
-    // Make a buffer for the square
-    squareBuffer = webGL.createBuffer();
-    webGL.bindBuffer(webGL.ARRAY_BUFFER, squareBuffer);
-    webGL.bufferData(webGL.ARRAY_BUFFER, squareVertices, webGL.STATIC_DRAW);
-  
-    // Get the program ready
-    webGL.useProgram(program);
-    program.position = webGL.getAttribLocation(program, "position");
-    webGL.enableVertexAttribArray(program.position);
-    webGL.vertexAttribPointer(program.position, dimensions, webGL.FLOAT, false, 0, 0);
-  
-    // Draw it!
-    webGL.drawArrays(webGL.TRIANGLES, 0, itemNum);
-  }
-}
+var webGL;
 
 function main() {
   // Start webGL on the canvas
   const canvas = document.getElementById("webgl-canvas");
-  const webGL = canvas.getContext("webgl");
+  webGL = canvas.getContext("webgl");
 
   // Make sure webGL works
   if (webGL === null) {
@@ -100,12 +36,58 @@ function main() {
   }
 
   // When the user clicks the fullscreen button, fullscreen the canvas
-  fullscreenButton = document.getElementById("fullscreen-button");
+  var fullscreenButton = document.getElementById("fullscreen-button");
   fullscreenButton.addEventListener("click", fullscreenCanvas);
 
-  // Draw a square
-  drawSquare();
-}
+  var getShaders = new Promise(function(resolve) {
+    var program = webGL.createProgram();
 
-// Start when the page has loaded
-window.onload = main;
+    // Load the vertex shader
+    fetch("vertex.glsl").then(function(response) {
+      var shader = webGL.createShader(webGL.VERTEX_SHADER);
+      webGL.shaderSource(shader, response.text());
+      webGL.compileShader(shader);
+      webGL.attachShader(program, shader);
+    });
+
+    // Load the fragment shader
+    fetch("fragment.glsl").then(function(response) {
+      var shader = webGL.createShader(webGL.FRAGMENT_SHADER);
+      webGL.shaderSource(shader, response.text());
+      webGL.compileShader(shader);
+      webGL.attachShader(program, shader);
+    });
+
+    webGL.linkProgram(program);
+    resolve(program)
+  });
+
+  getShaders.then(function(program) {
+
+    // Start the game
+    setInterval(function() {frameUpdate(program)}, 1000);
+  });
+
+  function frameUpdate(program) {
+    squareVertices = new Float32Array([
+      -0.5, 0.5, -0.5, -0.5, 0.5, -0.5
+      -0.5, 0.5, 0.5, 0.5, 0.5, -0.5
+    ]);
+    var dimensions = 2;
+    var itemNum = squareVertices.length / dimensions;
+
+    // Make a buffer for the square
+    squareBuffer = webGL.createBuffer();
+    webGL.bindBuffer(webGL.ARRAY_BUFFER, squareBuffer);
+    webGL.bufferData(webGL.ARRAY_BUFFER, squareVertices, webGL.STATIC_DRAW);
+
+    // Get the program ready
+    webGL.useProgram(program);
+    program.position = webGL.getAttribLocation(program, "position");
+    webGL.enableVertexAttribArray(program.position);
+    webGL.vertexAttribPointer(program.position, dimensions, webGL.FLOAT, false, 0, 0);
+
+    // Draw it!
+    webGL.drawArrays(webGL.TRIANGLES, 0, itemNum);
+  }
+}
