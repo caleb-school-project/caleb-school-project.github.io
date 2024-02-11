02cb81061d6166229f2ca6dc8493783f0de9b67d
Caleb Jeffries <caleb.jeffries.2010@gmail.com>
2024-02-11 11:53:44 -0400

    Add colliders

--- engine/physics.js
+++ engine/physics.js
@@ -15,17 +15,36 @@ function PhysicsObject(shapeObj) {
     this.forces = addVectors(this.forces, newForce);
   }
   this.frameUpdate = function() {
-    shapeCoords = new Array(this.shape.triangles.flat().length / 2).fill().map((_, i) => {
-      return this.shape.triangles.flat().slice(i * 2, (i + 1) * 2);
-    });
+    var move = (velocity) => {
+      shapeCoords = new Array(this.shape.triangles.flat().length / 2).fill().map((_, i) => {
+        return this.shape.triangles.flat().slice(i * 2, (i + 1) * 2);
+      });
+      for(var i = 0; i < shapeCoords.length; i++) {
+        shapeCoords[i] = addVectors(shapeCoords[i], velocity);
+      }
+      this.shape.triangles = shapeCoords;
+      for (i = 0; i < this.shape.colliders.length; i++) {
+        this.shape.colliders[i][0] += velocity[0];
+        this.shape.colliders[i][1] += velocity[1];
+      }
+    }
     var acceleration = [];
     for(var i = 0; i < this.forces.length; i++) {
       acceleration[i] = this.forces[i] / this.mass;
     }
     this.velocity = addVectors(this.velocity, acceleration);
-    for(var i = 0; i < shapeCoords.length; i++) {
-      shapeCoords[i] = addVectors(shapeCoords[i], this.velocity);
+    move(this.velocity);
+    for (var i = 0; i < objects.length; i++) {
+      if (objects[i].shape.colliders == this.shape.colliders) {
+        continue;
+      }
+      for (var collidernum = 0; collidernum < objects[i].shape.colliders.length; collidernum++) {
+        if (this.shape.colliders[0][0] + this.shape.colliders[0][2] > objects[i].shape.colliders[collidernum][0] && this.shape.colliders[0][0] < objects[i].shape.colliders[collidernum][0] + objects[i].shape.colliders[collidernum][2] && this.shape.colliders[0][1] + this.shape.colliders[0][3] > objects[i].shape.colliders[collidernum][1] && this.shape.colliders[0][1] < objects[i].shape.colliders[collidernum][1] + objects[i].shape.colliders[collidernum][3]) {
+          objects[i].velocity = [this.velocity[0] - objects[i].velocity[0], this.velocity[1] - objects[i].velocity[1]];
+          this.velocity = [objects[i].velocity[0] - this.velocity[0], objects[i].velocity[1] - this.velocity[1]];
+          console.log(this.velocity);
+        }
+      }
     }
-    this.shape.triangles = shapeCoords;
   }
 }
