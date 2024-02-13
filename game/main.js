58afae70bb2f490d02bc5af51a535f3e90fba497
Caleb Jeffries <caleb.jeffries.2010@gmail.com>
2024-02-12 21:40:59 -0400

    Add basic controls

--- game/main.js
+++ game/main.js
@@ -3,7 +3,7 @@ var floorPromise = fetch("shapes/floor.json").then(function(response) {
 }).then(function(response) {
   physicsObj = new PhysicsObject(response);
   physicsObj.lockPos = true;
-  objects.push(physicsObj) - 1;
+  return objects.push(physicsObj) - 1;
 });
 
 var squarePromise = fetch("shapes/square.json").then(function(response) {
@@ -11,5 +11,28 @@ var squarePromise = fetch("shapes/square.json").then(function(response) {
 }).then(function(response) {
   physicsObj = new PhysicsObject(response);
   physicsObj.addForce([0, -0.0001]);
-  objects.push(physicsObj) - 1;
+  return objects.push(physicsObj) - 1;
+}).then(function(objectIndex) {
+  window.onkeydown = function(e) {
+    if (e.key == "w" || e.key == "ArrowUp") {
+      objects[objectIndex].addForce([0, 0.0001]);
+    } else if (e.key == "a" || e.key == "ArrowLeft") {
+      objects[objectIndex].addForce([-0.0001, 0]);
+    } else if (e.key == "s" || e.key == "ArrowDown") {
+      objects[objectIndex].addForce([0, -0.0001]);
+    } else if (e.key == "d" || e.key == "ArrowRight") {
+      objects[objectIndex].addForce([0.0001, 0]);
+    }
+  };
+  window.onkeyup = function(e) {
+    if (e.key == "w" || e.key == "ArrowUp") {
+      objects[objectIndex].addForce([0, -0.0001]);
+    } else if (e.key == "a" || e.key == "ArrowLeft") {
+      objects[objectIndex].addForce([0.0001, 0]);
+    } else if (e.key == "s" || e.key == "ArrowDown") {
+      objects[objectIndex].addForce([0, 0.0001]);
+    } else if (e.key == "d" || e.key == "ArrowRight") {
+      objects[objectIndex].addForce([-0.0001, 0]);
+    }
+  };
 });
