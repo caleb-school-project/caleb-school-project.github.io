c11309a6b7f5a95cf23c8ef212febca2a0a74650
caleb-school-project <147189781+caleb-school-project@users.noreply.github.com>
2024-03-15 14:32:03 -0300

    Follow newton's 3rd law

--- engine/physics.js
+++ engine/physics.js
@@ -57,19 +57,19 @@ function PhysicsObject(shapeObj) {
           var otherNewMomentum = addVectors(otherMomentum, [thisMomentum[0] - otherMomentum[0], thisMomentum[1] - otherMomentum[1]]);
           var thisNewMomentum = addVectors(thisMomentum, [otherMomentum[0] - thisMomentum[0], otherMomentum[1] - thisMomentum[1]]);
           if (this.lockX) {
-            otherNewMomentum[0] += thisNewMomentum[0];
+            otherNewMomentum[0] -= thisNewMomentum[0];
             thisNewMomentum[0] = 0;
           }
           if (this.lockY) {
-            otherNewMomentum[1] += thisNewMomentum[1];
+            otherNewMomentum[1] -= -thisNewMomentum[1];
             thisNewMomentum[1] = 0;
           }
           if (objects[i].lockX) {
-            thisNewMomentum[0] += otherNewMomentum[0];
+            thisNewMomentum[0] -= -otherNewMomentum[0];
             otherNewMomentum[0] = 0;
           }
           if (objects[i].lockY) {
-            thisNewMomentum[1] += otherNewMomentum[1];
+            thisNewMomentum[1] -= -otherNewMomentum[1];
             otherNewMomentum[1] = 0;
           }
           var otherVelocity = [otherNewMomentum[0] / objects[i].mass, otherNewMomentum[1] / objects[i].mass];
