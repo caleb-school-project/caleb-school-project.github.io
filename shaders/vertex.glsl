02a0f7f871fa1773bc336b84571c861558d32439
caleb-school-project <147189781+caleb-school-project@users.noreply.github.com>
2024-05-31 14:21:48 -0300

    Improve the shader with aspect

--- shaders/vertex.glsl
+++ shaders/vertex.glsl
@@ -7,7 +7,7 @@ varying lowp vec4 positionWithAspect;
 varying lowp vec4 vcolor;
 
 void main() {
-  positionWithAspect = vec4((position.x / aspect) + camerashift.x, position.y + camerashift.y, position.z, position.w);
+  positionWithAspect = vec4((position.x + camerashift.x / aspect), position.y + camerashift.y, position.z, position.w);
   gl_Position = positionWithAspect;
   vcolor = color;
 }
